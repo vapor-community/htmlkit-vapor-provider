2a4ee0a33b79d444f828d3937bbdd0d649a84b61
Mats Eikeland Mollestad <mem@mollestad.no>
2019-12-30 11:46:59 +0100

    [HTMLKit] Updated to new htmlkit versoin

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/vapor-community/HTMLKit.git", from: "2.0.0-beta.1"),
+        .package(url: "https://github.com/vapor-community/HTMLKit.git", from: "2.0.0-beta.2"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3"),
     ],
     targets: [
