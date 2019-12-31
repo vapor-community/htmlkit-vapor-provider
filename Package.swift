0077fc281300c1d50782a5f537f02799af3397da
TheCB4 <cavelle@thecb4.io>
2019-12-31 12:23:14 -0500

    added macOS platform minimum for Vapor 4

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "HTMLKit-Vapor-Provider",
+    platforms: [
+      .macOS(.v10_14),
+    ],
+
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
