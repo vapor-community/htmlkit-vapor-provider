d90e2f2e13614219b8710a843e3c02fafc706a2a
Mattes Mohr <mail@mattesmohr.de>
2022-04-16 17:30:31 +0200

    Add test functions to test the async/await integration.

--- Tests/HTMLKitVaporProviderTests/IntegrationTests.swift
+++ Tests/HTMLKitVaporProviderTests/IntegrationTests.swift
@@ -27,8 +27,41 @@ final class IntegrationTests: XCTestCase {
         
         try app.htmlkit.add(view: TestPage())
         
-        app.get("test") { request in
-            return try request.htmlkit.render(TestPage.self)
+        app.get("test") { request -> EventLoopFuture<Vapor.View> in
+            
+            request.eventLoop.submit {
+                return try request.htmlkit.render(view: TestPage.self)
+            }
+        }
+        
+        try app.test(.GET, "test") { response in
+            XCTAssertEqual(response.status, .ok)
+            XCTAssertEqual(response.body.string,
+                            """
+                            <!DOCTYPE html>\
+                            <html>\
+                            <head>\
+                            <title>title</title>\
+                            </head>\
+                            <body>\
+                            </body>\
+                            </html>
+                            """
+            )
+        }
+    }
+    
+    @available(macOS 12, *)
+    func testAsyncIntegrationBySingleton() throws {
+        
+        let app = Application(.testing)
+        
+        defer { app.shutdown() }
+        
+        try app.htmlkit.add(view: TestPage())
+        
+        app.get("test") { request async throws -> Vapor.View in
+            return try request.htmlkit.render(view: TestPage.self)
         }
         
         try app.test(.GET, "test") { response in
@@ -54,8 +87,36 @@ final class IntegrationTests: XCTestCase {
         
         defer { app.shutdown() }
         
-        app.get("test") { request in
+        app.get("test") { request -> EventLoopFuture<Vapor.View> in
             return TestPage().render(for: request)
+        }
+        
+        try app.test(.GET, "test") { response in
+            XCTAssertEqual(response.status, .ok)
+            XCTAssertEqual(response.body.string,
+                            """
+                            <!DOCTYPE html>\
+                            <html>\
+                            <head>\
+                            <title>title</title>\
+                            </head>\
+                            <body>\
+                            </body>\
+                            </html>
+                            """
+            )
+        }
+    }
+    
+    @available(macOS 12, *)
+    func testAsyncIntegrationByStructure() throws {
+        
+        let app = Application(.testing)
+        
+        defer { app.shutdown() }
+        
+        app.get("test") { request async throws -> Vapor.View in
+            return try TestPage().render(for: request)
         }
         
         try app.test(.GET, "test") { response in
