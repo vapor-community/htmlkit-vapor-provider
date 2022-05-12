7d1225365357a222285fc35127df37333ef95c1b
Mattes Mohr <mail@mattesmohr.de>
2022-05-12 22:38:44 +0200

    Update the package to work with the concurrency build of Vapor (#10)
    * Update the package and remove the resolved-file.
    
    * Delete Package.resolved
    
    * Add render functions to work with the async/await build of Vapor.
    
    * Add test functions to test the async/await integration.
    
    * Add code comments
    
    * Relocate the class extensions

--- Sources/HTMLKitVaporProvider/Provider.swift
+++ Sources/HTMLKitVaporProvider/Provider.swift
@@ -1,142 +1,56 @@
+/*
+ Abstract:
+ The file contains a view provider for Vapor.
+ 
+ Authors:
+ - Mats Moll (https://github.com/matsmoll)
+ 
+ Contributors:
+ - Mattes Mohr (https://github.com/mattesmohr)
+ 
+ If you about to add something to the file, stick to the official documentation to keep the code consistent.
+ */
+
 import Vapor
 import NIO
 @_exported import HTMLKit
 
-/// The provider is  for
-///
-///
+/// The view provider.
 public class Provider: LifecycleHandler {
 
     public static var shared: Provider?
 
+    /// The view renderer.
     public var renderer = Renderer()
 
+    /// The path of the localization directory.
     public var localizationPath: String?
     
+    /// The default locale for the localization.
+    ///
+    /// If the variable is nil, it sets the english local as default.
     public var defaultLocale: String?
 
+    /// Creates a instance of the provider and adds to the lifecycle.
     public init(app: Application) {
         app.lifecycle.use(self)
     }
 
+    /// Adds a page to the view renderer.
     public func add<T: HTMLKit.Page>(view: T) throws {
         try renderer.add(view: view)
     }
 
+    /// Adds a view to the view renderer.
     public func add<T: HTMLKit.View>(view: T) throws {
         try renderer.add(view: view)
     }
 
+    /// Registers the localization.
     public func willBoot(_ application: Application) throws {
         
         if let localizationPath = localizationPath {
             try renderer.registerLocalization(atPath: localizationPath, defaultLocale: defaultLocale ?? "en")
         }
     }
-}
-
-extension Vapor.Application {
-
-    public var htmlkit: Provider {
-        
-        if let shared = Provider.shared {
-            return shared
-        }
-        
-        Provider.shared = .init(app: self)
-        
-        return Provider.shared!
-    }
-}
-
-extension Vapor.Request {
-    
-    public var htmlkit: Renderer {
-        
-        self.application.htmlkit.renderer
-    }
-}
-
-extension HTMLKit.View {
-    
-    public func render(with context: Context, for request: Request) -> EventLoopFuture<Vapor.View> {
-        
-        request.eventLoop.submit {
-            
-            do {
-                
-                return try request.htmlkit.render(view: Self.self, with: context)
-                
-            } catch Renderer.Errors.unableToFindFormula {
-                
-                try request.application.htmlkit.add(view: self)
-                
-                return try request.htmlkit.render(view: Self.self, with: context)
-            }
-        }
-    }
-}
-
-extension HTMLKit.Page {
-    
-    public func render(for request: Request) -> EventLoopFuture<Vapor.View> {
-        
-        request.eventLoop.submit {
-            
-            do {
-                
-                return try request.htmlkit.render(view: Self.self)
-                
-            } catch Renderer.Errors.unableToFindFormula {
-                
-                try request.application.htmlkit.add(view: self)
-                
-                return try request.htmlkit.render(view: Self.self)
-            }
-        }
-    }
-}
-
-extension HTMLKit.Renderer {
-    
-    public func render<T: HTMLKit.Page>(_ type: T.Type) throws -> Response {
-        
-        let page = try render(raw: type)
-        
-        return Response(headers: .init([("content-type", "text/html; charset=utf-8")]), body: .init(string: page))
-    }
-    
-    public func render<T: HTMLKit.View>(_ type: T.Type, with value: T.Context) throws -> Response {
-        
-        let view = try render(raw: type, with: value)
-        
-        return Response(headers: .init([("content-type", "text/html; charset=utf-8")]), body: .init(string: view))
-    }
-
-    public func render<T: HTMLKit.Page>(view type: T.Type) throws -> Vapor.View {
-        
-        let page = try render(raw: type)
-        
-        var buffer = ByteBufferAllocator().buffer(capacity: page.utf8.count)
-        buffer.writeString(page)
-        
-        return View(data: buffer)
-    }
-
-    public func render<T: HTMLKit.View>(view type: T.Type, with context: T.Context) throws -> Vapor.View {
-        
-        let view = try render(raw: type, with: context)
-        
-        var buffer = ByteBufferAllocator().buffer(capacity: view.utf8.count)
-        buffer.writeString(view)
-        
-        return View(data: buffer)
-    }
-}
-
-extension HTMLKit.Renderer.Errors: DebuggableError {
-    
-    public var identifier: String { "HTMLRenderer.Errors" }
-    
-    public var reason: String { self.errorDescription ?? "" }
 }
